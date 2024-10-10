2c994f72e5c434fb5deb4e0520b304e540fbd264
seokhyun <Ryu-Seok-Hyn@github.com>
2024-10-10 09:04:54 +0900

    석현 : 카카오 로그인 엔티티 유저로 들어가도록 수정.

--- src/main/java/com/keduit/dadog/repository/UserRepository.java
+++ src/main/java/com/keduit/dadog/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.keduit.dadog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
+    User findByUserId(String userId); // 사용자 ID로 사용자 찾기
 }
