43645d90a45c6348b6320a0e9dcd21ba4bfb5c8a
Moonssung <mari9426@naver.com>
2024-10-10 12:29:41 +0900

    성현 : lostList.html 구현

--- src/main/java/com/keduit/dadog/controller/LostController.java
+++ src/main/java/com/keduit/dadog/controller/LostController.java
@@ -1,9 +1,13 @@
 package com.keduit.dadog.controller;
 
 import com.keduit.dadog.dto.LostDTO;
+import com.keduit.dadog.dto.LostSearchDTO;
 import com.keduit.dadog.entity.Lost;
 import com.keduit.dadog.service.LostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/dadog/lost")
@@ -21,6 +26,23 @@ import java.security.Principal;
 public class LostController {
 
     private final LostService lostService;
+
+    @GetMapping({"/list","/list/{page}"})
+    public String LostList(LostSearchDTO lostSearchDTO,
+                           @PathVariable("page") Optional<Integer> page,
+                           Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent()? page.get() : 0, 20);
+        Page<Lost> lostList = lostService.getLostList(lostSearchDTO, pageable);
+        System.out.println("---------------------------------");
+        System.out.println(lostSearchDTO.getSearchBy());
+        System.out.println(lostSearchDTO.getSearchQuery());
+        System.out.println(lostList.getTotalElements());
+        model.addAttribute("lostSearchDTO", lostSearchDTO);
+        model.addAttribute("maxPage",5);
+        model.addAttribute("lostList", lostList);
+        System.out.println(lostList);
+        return "lost/lostList";
+    }
 
     @GetMapping("/add")
     public String addLost(Principal principal, Model model) {
@@ -32,8 +54,6 @@ public class LostController {
 
     @PostMapping("/add")
     public String addLost(LostDTO lostDTO, Principal principal, MultipartFile lostImg, Model model, BindingResult bindingResult){
-        System.out.println("boardDTO ------> " + lostDTO);
-        System.out.println("img --------------> " + lostImg);
         if(bindingResult.hasErrors()) {
             System.out.println(bindingResult.getFieldError().getDefaultMessage());
             return "redirect:/dadog/lost/add";
@@ -49,5 +69,13 @@ public class LostController {
             return "redirect:/dadog/lost/add";
         }
         return "redirect:/dadog/main";
+    }
+
+    @GetMapping("/{lostNo}")
+    public String getLost(Principal principal, @PathVariable("lostNo") Long lostNo, Model model) {
+       Lost lost =  lostService.getLost(lostNo);
+       model.addAttribute("lost", lost);
+       model.addAttribute("userId", principal.getName());
+       return "lost/lostDtl";
     }
 }
--- src/main/java/com/keduit/dadog/service/LostService.java
+++ src/main/java/com/keduit/dadog/service/LostService.java
@@ -1,6 +1,7 @@
 package com.keduit.dadog.service;
 
 import com.keduit.dadog.dto.LostDTO;
+import com.keduit.dadog.dto.LostSearchDTO;
 import com.keduit.dadog.entity.Lost;
 import com.keduit.dadog.entity.User;
 import com.keduit.dadog.repository.LostRepository;
@@ -9,9 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.codehaus.groovy.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -24,11 +30,27 @@ public class LostService {
     @Value("${lostImgLocation}")
     private String lostImgLocation;
 
+    //실종신고 게시판
+    @Transactional(readOnly = true)
+    public Page<Lost> getLostList(LostSearchDTO lostSearchDTO, Pageable pageable) {
+        return lostRepository.getAdoptListPage(lostSearchDTO, pageable);
+    }
+
+    public Lost getLost(Long lostNo){
+        return lostRepository.findById(lostNo).orElseThrow(EntityNotFoundException::new);
+    }
+
+
+
     //실종신고 등록
     public Long addLost(LostDTO lostDTO, String userName, MultipartFile lostImg) throws Exception{
         User user = userRepository.findByUserId(userName);
+        if(user == null) {
+            System.out.println("--------------> 여기");
+            user = userRepository.findByUserEmail(userName);
+        }
+
         Lost lost = Lost.createLost(lostDTO, userName, user);
-        //TODO : lostImgUrl, lostOriName, lostFileName 넣어야함
         String originalFileName =  lostImg.getOriginalFilename();
         String imgUrl = "";
         String imgName = "";
@@ -37,7 +59,6 @@ public class LostService {
             imgName = fileService.uploadLostFile(lostImgLocation, originalFileName, lostImg.getBytes());
             imgUrl = "/images/lost/" + imgName;
         }
-
 
         lost.setLostOriName(originalFileName);
         lost.setLostImgUrl(imgUrl);
